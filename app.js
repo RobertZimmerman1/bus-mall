69b05623b280ef593028fe28ef13829f2f44728d
ROBERT ZIMMERMAN <ROBERT.R.ZIMMERMAN@GMAIL.COM>
2021-06-22 21:28:40 -0700

    started trying to add chart

--- app.js
+++ app.js
@@ -9,19 +9,57 @@ const leftH3Elem = document.getElementById('image_left_h3');
 const centerH3Elem = document.getElementById('image_center_h3');
 const rightH3Elem = document.getElementById('image_right_h3');
 const productLikesElem = document.getElementById('product_likes');
-
+Product.allProducts = [];
 let likeCounter = 0;
+
 let leftImage = null;
 let centerImage = null;
 let rightImage = null;
 
-Product.allProducts = [];
+//Chart Script ----------------Currently Broken---------------------------------
+// const ctx = document.getElementById('myChart').getContext('2d');
 
-//Event Listeners ---------------------------------------------------------
-imageSelectorElem.addEventListener('click', handleClick);
+// const data = [1, 2, 3, 4, 5];
+// const labelColors = ['red', 'blue', 'green', 'pink', 'yellow'];
+
+// const  myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: labelColors,
+//         datasets: [{
+//             label: '# of Votes',
+//             data: data,
+//             backgroundColor: labelColors
+//         }]
+//             [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             // ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// });
 
 //Constructor Function ---------------------------------------------------------
-
 function Product(name, image) {
   this.name = name;
   this.image = image;
@@ -31,96 +69,100 @@ function Product(name, image) {
 }
 
 //Prototype Methods ---------------------------------------------------------
-Product.prototype.renderSingleProduct = function (imgPosition, h3Position) {
+Product.prototype.renderSingleProduct = function (imgPosition) {
   imgPosition.src = this.image;
-  h3Position.textContent = this.name;
+  // h3Position.textContent = this.name;
   this.timesShown++;
 }
 
 //Global Functions ---------------------------------------------------------
-
 function getRandomIndex() {
   return Math.floor(Math.random() * Math.floor(Product.allProducts.length));
 }
 
 function chooseThreeProducts() {
-  let lastProducts = [currentLeftProduct, currentCenterProduct, currentRightProduct];
+  let lastProducts = [leftImage, centerImage, rightImage];
 
-  while (lastProducts.includes(currentLeftProduct)) {
+  while (lastProducts.includes(leftImage)) {
     let randomIndex = getRandomIndex();
-    currentLeftProduct = Product.allProducts[randomIndex];
+    leftImage = Product.allProducts[randomIndex];
   }
-  lastProducts.push(currentLeftProduct);
+  lastProducts.push(leftImage);
 
-  while (lastProducts.includes(currentCenterProduct)) {
+  while (lastProducts.includes(centerImage)) {
     let randomIndex = getRandomIndex();
-    currentCenterProduct = Product.allProducts[randomIndex];
+    centerImage = Product.allProducts[randomIndex];
   }
-  lastProducts.push(currentCenterProduct);
+  lastProducts.push(centerImage);
 
-  while (lastProducts.includes(currentRightProduct)) {
+  while (lastProducts.includes(rightImage)) {
     let randomIndex = getRandomIndex();
-    currentRightProduct = Product.allProducts[randomIndex];
+    rightImage = Product.allProducts[randomIndex];
   }
-  lastProducts.push(currentRightProduct);
+  lastProducts.push(rightImage);
+
+  leftImage.renderSingleProduct(leftImgElem);
+  centerImage.renderSingleProduct(centerImgElem);
+  rightImage.renderSingleProduct(rightImgElem);
 }
 
-currentLeftProduct.renderSingleProduct(leftImgElem);
-currentCenterProduct.renderSingleProduct(centerImgElem);
-currentRightProduct.renderSingleProduct(rightImgElem);
-
-
-// function renderResults() {
-//   productLikesElem.innerHTML = '';
-
-//   for (let i = 0; i < Product.allProducts.length; i++) {
-//     let product = Product.allProducts[i];
-//     let liElem = document.createElement('li');
-//     liElem.textContent = `${product.name}: ${product.likes}`;
-//     productLikesElem.appendChild(liElem);
-//   }
-// }
+function createProducts() {
+  new Product('R2 D2 Tote', './img/bag.jpg');
+  new Product('Banana Slicer', './img/banana.jpg');
+  new Product('iPad Holder', './img/bathroom.jpg');
+  new Product('Boot Sandals', './img/boots.jpg');
+  new Product('Breakfast Kit', './img/breakfast.jpg');
+  new Product('Bubble Gum Meatballs', './img/bubblegum.jpg');
+  new Product('Chair', './img/chair.jpg');
+  new Product('Cthulhu Action Figure', './img/cthulhu.jpg');
+  new Product('Duck Lips', './img/dog-duck.jpg');
+  new Product('Dragon Meat', './img/dragon.jpg');
+  new Product('Pen Utensils', './img/pen.jpg');
+  new Product('Pet Sweeper', './img/pet-sweep.jpg');
+  new Product('Pizza Scissors', './img/scissors.jpg');
+  new Product('Shark Sleeping Bag', './img/shark.jpg');
+  new Product('Baby Sweeper', './img/sweep.png');
+  new Product('Taun Taun Sleeping Bag', './img/tauntaun.jpg');
+  new Product('Unicorn Meat', './img/unicorn.jpg');
+  new Product('Watering Can', './img/water-can.jpg');
+  new Product('Wine Glass', './img/wine-glass.jpg');
+  }
 
 function handleClick(event) {
-  //alert(event.target.id); play with and see if this does anything
 
-  let id = event.target.id
-  if (id === 'image_left_picture' || id === 'image_center_picture' || id === 'image_right_picture') {
+  let id = (event.target.id);
+  let select = null;
+
+  if (id === 'image_left_picture') {
+    select = leftImage;
+  } else if (id === 'image_center_picture') {
+    select = centerImage;
+  } else if (id === 'image_right_picture') {
+    select = rightImage;
+  }
+
+  if (select) {
+    select.likes++;
     likeCounter++;
-    if (id === 'image_left_picture') {
-      leftImage.likes++;
-    } else if (id === 'image_center_picture') {
-      centerImage.like++;
-    } else {
-      rightImage++;
+    chooseThreeProducts();
+  }
+  if (likeCounter === 25){
+    productLikesElem.innerHTML = '';
+
+    for (let i = 0; i < Product.allProducts.length; i++) {
+      let product = Product.allProducts[i];
+      let liElem = document.createElement('li');
+      liElem.textContent = `${product.name}: ${product.likes}`;
+      productLikesElem.appendChild(liElem);
     }
-  }
-  if (likeCounter === 4) {
-    renderResults();
     imageSelectorElem.removeEventListener('click', handleClick);
-  }
+}
 }
 
+//Event Listeners ---------------------------------------------------------
+imageSelectorElem.addEventListener('click', handleClick);
 
 //Call Functions ---------------------------------------------------------
-new Product('R2 D2 Tote', './images/bag.jpg');
-new Product('Banana Slicer', './images/banana.jpg');
-new Product('iPad Holder', './images/bathroom.jpg');
-new Product('Boot Sandals', './images/boots.jpg');
-new Product('Breakfast Kit', './images/breakfast.jpg');
-new Product('Bubble Gum Meatballs', './images/bubblegum.jpg');
-new Product('Chair', './images/chair.jpg');
-new Product('Cthulhu Action Figure', './images/cthulhu.jpg');
-new Product('Duck Lips', './images/dog-duck.jpg');
-new Product('Dragon Meat', './images/dragon.jpg');
-new Product('Pen Utensils', './images/pen.jpg');
-new Product('Pet Sweeper', './images/pet-sweep.jpg');
-new Product('Pizza Scissors', './images/scissors.jpg');
-new Product('Shark Sleeping Bag', './images/shark.jpg');
-new Product('Baby Sweeper', './images/sweep.png');
-new Product('Taun Taun Sleeping Bag', './images/tauntaun.jpg');
-new Product('Unicorn Meat', './images/unicorn.jpg');
-new Product('Watering Can', './images/water-can.jpg');
-new Product('Wine Glass', './images/wine-glass.jpg');
+createProducts();
+chooseThreeProducts();
 
-pickThreeImages();
